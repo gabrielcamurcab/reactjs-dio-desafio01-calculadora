67c212fa18c75078bad19cb8333b0c6aa82c0e48
Gabriel Camurça <gabriel@brtech.dev>
2023-11-24 14:42:20 -0300

    Create Container && Input

---
+++ src/components/Input/index.js
@@ -0,0 +1,13 @@
+import { InputContainer } from "./styles";
+
+const Input = () => {
+    return (
+      <div className="App">
+        <InputContainer>
+          <input />
+        </InputContainer>
+      </div>
+    );
+  }
+
+export default Input;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #004d66;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  color: #ffffff;
+
+  font-size: 24px;
+  font-family: sans-serif;
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #004d66;
+    border: 0;
+
+    text-align: right;
+    color: #ffffff;
+
+    padding: 0 10px;
+
+    font-size: 24px;
+    font-family: sans-serif;
+  }
+`;
