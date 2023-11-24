1607131c6a45e016a08f53e860a0b6a33999313f
Gabriel Camurça <gabriel@brtech.dev>
2023-11-24 17:34:56 -0300

    Implements sum and substract functions

--- src/App.js
+++ src/App.js
@@ -6,40 +6,94 @@ import { useState } from "react";
 
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
 
   const handleOnClear = () => {
     setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
   }
+
   const handleAddNumber = (number) => {
     setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`)
   }
+
+  const handleSumNumbers = () => {
+
+    if (firstNumber === '0') {
+      setFirstNumber(currentNumber);
+      setCurrentNumber('0');
+      setOperation('+');
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(sum);
+      setOperation('');
+    }
+
+  }
+
+  const handleSubstractNumbers = () => {
+
+    if (firstNumber === '0') {
+      setFirstNumber(currentNumber);
+      setCurrentNumber('0');
+      setOperation('-');
+    } else {
+      const substract = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(substract);
+      setOperation('');
+    }
+
+  }
+
+  const handleEquals = () => {
+
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== 0) {
+      switch (operation) {
+        case '+':
+          handleSumNumbers();
+          break;
+        case '-':
+          handleSubstractNumbers();
+          break;
+        default:
+          break;
+      }
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(sum);
+    }
+
+  }
+
   return (
     <Container>
       <Content>
-        <Input value={currentNumber}/>
+        <Input value={currentNumber} />
         <Row>
-          <Button label="x" onClick={() => handleAddNumber('x')}/>
-          <Button label="/" onClick={() => handleAddNumber('/')}/>
-          <Button label="C" onClick={() => handleOnClear()}/>
-          <Button label="X"/>
+          <Button label="x" onClick={() => handleAddNumber('x')} />
+          <Button label="/" onClick={() => handleAddNumber('/')} />
+          <Button label="C" onClick={() => handleOnClear()} />
+          <Button label="X" />
         </Row>
         <Row>
-          <Button label="7" onClick={() => handleAddNumber('7')}/>
-          <Button label="8" onClick={() => handleAddNumber('8')}/>
-          <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={() => handleAddNumber('-')}/>
+          <Button label="7" onClick={() => handleAddNumber('7')} />
+          <Button label="8" onClick={() => handleAddNumber('8')} />
+          <Button label="9" onClick={() => handleAddNumber('9')} />
+          <Button label="-" onClick={handleSubstractNumbers} />
         </Row>
         <Row>
-          <Button label="4" onClick={() => handleAddNumber('4')}/>
-          <Button label="5" onClick={() => handleAddNumber('5')}/>
-          <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={() => handleAddNumber('+')}/>
+          <Button label="4" onClick={() => handleAddNumber('4')} />
+          <Button label="5" onClick={() => handleAddNumber('5')} />
+          <Button label="6" onClick={() => handleAddNumber('6')} />
+          <Button label="+" onClick={handleSumNumbers} />
         </Row>
         <Row>
-          <Button label="1" onClick={() => handleAddNumber('1')}/>
-          <Button label="2" onClick={() => handleAddNumber('2')}/>
-          <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={() => handleAddNumber('=')}/>
+          <Button label="1" onClick={() => handleAddNumber('1')} />
+          <Button label="2" onClick={() => handleAddNumber('2')} />
+          <Button label="3" onClick={() => handleAddNumber('3')} />
+          <Button label="=" onClick={handleEquals} />
         </Row>
       </Content>
     </Container>
